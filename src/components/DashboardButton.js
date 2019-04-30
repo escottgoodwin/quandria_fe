99610a791dafcb73cb502f36d8a3c1c02590224a
Evan Goodwin <escottgoodwin@gmail.com>
2019-04-29 17:11:51 -0700

    fixed no user for nav button

--- src/components/DashboardButton.js
+++ src/components/DashboardButton.js
@@ -6,31 +6,32 @@ import {Link} from 'react-router-dom'
 
 class DashboardButton extends Component {
 
-  navSwitch = (role) => {
-    switch (role) {
-      case 'TEACHER':
-        return <Link to="/teacher_dashboard"> <Button size='large' basic color='blue'>Dashboard</Button></Link>
-      case 'STUDENT':
-      return <Link to="/student_dashboard"> <Button size='large' basic color='blue'>Dashboard</Button></Link>
-      case 'ADMIN':
-        return <Link to="/admin_dashboard"> <Button size='large' basic color='blue'>Dashboard</Button></Link>
-      case 'QUANDRIA':
-          return <Link to="/quandria_dashboard"> <Button size='large' basic color='blue'>Dashboard</Button></Link>
-      default:
-        return <Link to="/sign_up"><Button basic color='blue'>Sign Up</Button></Link>
-    }
+  navSwitch = (user) => {
+    if (!!user) {
+      switch (user.role) {
+        case 'TEACHER':
+          return <Link to="/teacher_dashboard"> <Button size='large' basic color='blue'>Dashboard</Button></Link>
+        case 'STUDENT':
+        return <Link to="/student_dashboard"> <Button size='large' basic color='blue'>Dashboard</Button></Link>
+        case 'ADMIN':
+          return <Link to="/admin_dashboard"> <Button size='large' basic color='blue'>Dashboard</Button></Link>
+        case 'QUANDRIA':
+            return <Link to="/quandria_dashboard"> <Button size='large' basic color='blue'>Dashboard</Button></Link>
+        default:
+          return <Link to="/sign_up"><Button basic color='blue'>Sign Up</Button></Link>
+      }
+  } else {
+    return <Link to="/sign_up"><Button basic color='blue'>Sign Up</Button></Link>
   }
+}
 
   render() {
-
+    
     return (
       <>
-      {this.navSwitch(JSON.parse(sessionStorage.getItem('user')).role)}
+      {this.navSwitch(JSON.parse(sessionStorage.getItem('user')))}
       </>
     )
-
-
-
 
 }
 }
